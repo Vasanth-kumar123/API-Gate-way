35069845c6063359e1d9fa443fec03ce106232ea
Vavilipalli Vasanthakumar <Vasanthakumar.v@keka.com>
2020-11-20 18:45:43 +0530

    Add project files.

---
+++ Assets/Controllers/AssetsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AssetsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            //var customers = await _context.Products.ToListAsync();
+            //if (customers == null) return NotFound();
+            return Ok("Assets Microservice executed !");
+        }
+    }
+}
---
+++ Documents/Controllers/DocumentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Documents.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            //var customers = await _context.Products.ToListAsync();
+            //if (customers == null) return NotFound();
+            return Ok("Documents Microservice executed !");
+        }
+    }
+}
